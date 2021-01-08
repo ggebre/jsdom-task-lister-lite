afcaf3717786e40414a732b1b7ac60dac26a01f1
Getu Gebre <getu.gebre.gg@gmail.com>
2021-01-08 15:17:19 -0600

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,96 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+    
+
+
+  const input = document.getElementById("new-task-description");
+  const toDoList = document.getElementById("tasks");
+  const form = document.getElementById("create-task-form");
+  const colors = ['red', 'yellow', 'green'];
+  let todoListArray = [];
+  // let todoListArray = {};
+
+
+  const color = {yellow : "#FFFF00", red : "#FF0000", green : "#00FF00"};
+  // console.log(button[0].value)
+  const select = document.createElement('select');
+    select.setAttribute('name', 'priority');
+    select.setAttribute('id', 'priority');
+    for(let i=0; i < colors.length; i++){
+      select.innerHTML += `<option value=${i}> ${colors[i]} </option>`
+    }
+    // colors.forEach( color => {
+    //   select.innerHTML += `<option value=${color}> ${color} </option>`
+    // });
+  form.appendChild(select);
+  
+  form.addEventListener('submit', logSubmit, false);
+
+  
+  function logSubmit(event) {
+    toDoList.innerText = "";
+    inputValue = input.value 
+    todoListArray.push([`${inputValue}`,  `${select.value}`]);
+    // todoListArray[`${inputValue}`] = `${select.value}`;
+    // sort array here 
+    todoListArray.sort((a, b) => {
+      if (a[1] > b[1]) {
+        return 1;
+      };
+      if (a[1] < b[1]){
+        return -1;
+      }
+      return 0;
+    });
+    todoListArray.forEach(entry => {
+    // Object.entries(todoListArray).forEach(entry => {
+      const [key, value] = entry;
+      let textColor = color[colors[parseInt(value)]];
+      let list = listCreate(key, textColor);
+      let button = buttonCreate('X', 'delete');
+      let editbutton = buttonCreate('Edit', 'edit');
+      
+      list.appendChild(button);
+      list.appendChild(editbutton);
+      toDoList.appendChild(list);
+    });
+
+
+    function buttonCreate(text, className) {
+      let button = document.createElement('button');
+      button.textContent = text;
+      button.setAttribute('class', `${className}`);
+      return button
+    }
+
+    function listCreate(text, textcolor){
+      let list = document.createElement('li');
+      list.textContent = text;
+      list.style.color = `${textcolor}`;
+      return list
+    }
+
+    
+    // toDoList.innerHTML += `<li style=color:${color[select.value]}> ${input.value} <button id=${input.value}> X </button></li>`
+  
+   let deleteButton = document.getElementsByClassName('delete');
+   let editButton = document.getElementsByClassName('edit');
+    let list = document.querySelectorAll('li');
+    for (let i=0; i < deleteButton.length; i++){
+      deleteButton[i].addEventListener('click', () => {
+        todoListArray.splice(i, 1);
+        toDoList.removeChild(list[i]);
+        
+      });
+      // remove from array
+    } 
+
+    for (let i=0; i < deleteButton.length; i++){
+      // deleteButton[i].addEventListener('click', () => toDoList.removeChild(list[i]));
+      // deleteButton[i].addEventListener('click', () => toDoList.removeChild(list[i]));
+      console.log('hello');
+    } 
+    event.preventDefault();
+  }
 });
+
