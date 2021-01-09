49c520d3fe9df4436ddb5810a3d29e1a20effa05
Getu Gebre <getu.gebre.gg@gmail.com>
2021-01-08 19:58:06 -0600

    refactor

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const input = document.getElementById("new-task-description");
   const toDoList = document.getElementById("tasks");
   const form = document.getElementById("create-task-form");
+  let divList = document.getElementById('list');
   const colors = ['red', 'yellow', 'green'];
+  let deleteButton;
+  let editButton;
+  let list;
   let todoListArray = [];
   // let todoListArray = {};
 
@@ -19,9 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
     for(let i=0; i < colors.length; i++){
       select.innerHTML += `<option value=${i}> ${colors[i]} </option>`
     }
-    // colors.forEach( color => {
-    //   select.innerHTML += `<option value=${color}> ${color} </option>`
-    // });
+    
   form.appendChild(select);
   
   form.addEventListener('submit', logSubmit, false);
@@ -30,10 +32,57 @@ document.addEventListener("DOMContentLoaded", () => {
   function logSubmit(event) {
     toDoList.innerText = "";
     inputValue = input.value 
-    todoListArray.push([`${inputValue}`,  `${select.value}`]);
+    if (inputValue != ""){
+      todoListArray.push([`${inputValue}`,  `${select.value}`]);
+    }
     // todoListArray[`${inputValue}`] = `${select.value}`;
     // sort array here 
-    todoListArray.sort((a, b) => {
+    sortArray();
+    if (inputValue != "") {
+      displayList();
+    }
+    
+  
+    deleteButton = document.getElementsByClassName('delete');
+    editButton = document.getElementsByClassName('edit');
+    list = document.querySelectorAll('li');
+
+    for (let i=0; i < deleteButton.length; i++){
+      deleteButton[i].addEventListener('click', () => {
+        todoListArray.splice(i, 1);
+        toDoList.removeChild(list[i]);
+      });
+      // remove from array
+    } 
+
+    for (let i=0; i < deleteButton.length; i++){
+      editButton[i].addEventListener('click', () => {
+        // create a form with an texfield input 
+        // when this form is submitted, it should update the value of the list ...
+        createEditForm(list[i].innerText.slice(0,-5));
+      });
+      
+    } 
+    event.preventDefault();
+  }
+
+  function displayList() {
+    todoListArray.forEach(entry => {
+      // Object.entries(todoListArray).forEach(entry => {
+        const [key, value] = entry;
+        let textColor = color[colors[parseInt(value)]];
+        let list = listCreate(key, textColor);
+        let button = buttonCreate('X', 'delete');
+        let editbutton = buttonCreate('Edit', 'edit');
+        
+        list.appendChild(button);
+        list.appendChild(editbutton);
+        toDoList.appendChild(list);
+      });
+  }
+  function sortArray() {
+    todoListArray.sort( 
+      (a, b) => {
       if (a[1] > b[1]) {
         return 1;
       };
@@ -41,56 +90,36 @@ document.addEventListener("DOMContentLoaded", () => {
         return -1;
       }
       return 0;
-    });
-    todoListArray.forEach(entry => {
-    // Object.entries(todoListArray).forEach(entry => {
-      const [key, value] = entry;
-      let textColor = color[colors[parseInt(value)]];
-      let list = listCreate(key, textColor);
-      let button = buttonCreate('X', 'delete');
-      let editbutton = buttonCreate('Edit', 'edit');
-      
-      list.appendChild(button);
-      list.appendChild(editbutton);
-      toDoList.appendChild(list);
-    });
-
-
-    function buttonCreate(text, className) {
-      let button = document.createElement('button');
-      button.textContent = text;
-      button.setAttribute('class', `${className}`);
-      return button
     }
+    );
+  }
+  function buttonCreate(text, className) {
+    let button = document.createElement('button');
+    button.textContent = text;
+    button.setAttribute('class', `${className}`);
+    return button
+  }
 
-    function listCreate(text, textcolor){
-      let list = document.createElement('li');
-      list.textContent = text;
-      list.style.color = `${textcolor}`;
-      return list
-    }
+  function listCreate(text, textcolor){
+    let list = document.createElement('li');
+    list.textContent = text;
+    list.style.color = `${textcolor}`;
+    return list
+  }
 
+  function createEditForm(placeholder) {
     
-    // toDoList.innerHTML += `<li style=color:${color[select.value]}> ${input.value} <button id=${input.value}> X </button></li>`
-  
-   let deleteButton = document.getElementsByClassName('delete');
-   let editButton = document.getElementsByClassName('edit');
-    let list = document.querySelectorAll('li');
-    for (let i=0; i < deleteButton.length; i++){
-      deleteButton[i].addEventListener('click', () => {
-        todoListArray.splice(i, 1);
-        toDoList.removeChild(list[i]);
-        
-      });
-      // remove from array
-    } 
+    let form = document.createElement('form');
+    let input = document.createElement('input');
+    let submit = document.createElement('input');
 
-    for (let i=0; i < deleteButton.length; i++){
-      // deleteButton[i].addEventListener('click', () => toDoList.removeChild(list[i]));
-      // deleteButton[i].addEventListener('click', () => toDoList.removeChild(list[i]));
-      console.log('hello');
-    } 
-    event.preventDefault();
+    input.type = "text";
+    input.placeholder = placeholder;
+    input.id = 'edit_field';
+    submit.type = 'submit';
+    form.appendChild(input);
+    form.appendChild(submit);
+    divList.appendChild(form);
   }
 });
 
